0ecbd40905f6c1f2d026c7ad35a45f0eac8e9b43
csstransky <csstransky@gmail.com>
2019-03-30 18:32:17 -0400

    Updated skills and spells to include more stuff

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -45,14 +45,22 @@ elf_wpns = ["Longsword", "Shortsword", "Shortbow", "Longbow"]
 Repo.insert!(%Race{name: "Elf", dex_bonus: 2, int_bonus: 1, size: "medium",
                   prof_array: elf_profs, weapon_prof_array: elf_wpns})
 ###### SPELLS ######
+Repo.insert!(%Spell{name: "Fire Bolt", dice: "1d10",
+        level_req: 1, target: "enemy", type: "fire", mp_cost: 0})
 Repo.insert!(%Spell{name: "Magic Missle", dice: "1d4", dice_bonus: 1,
-        level_req: 1, target: "enemies", type: "force"})
+        level_req: 1, target: "enemies", type: "force", mp_cost: 0})
+Repo.insert!(%Spell{name: "Cure Wounds", dice: "1d8",
+        level_req: 1, target: "member", type: "heal", mp_cost: 1})
+Repo.insert!(%Spell{name: "Shield of Faith", dice: "2d1",
+        level_req: 1, target: "member", type: "buff", buff_stat: "AC", mp_cost: 1})
 
 ###### SKILLS ######
-Repo.insert!(%Skill{name: "Short Rest", level_req: 0, type: "rest",
-              sp_cost: 0, target: "self"})
+Repo.insert!(%Skill{name: "Short Rest", level_req: 1, type: "rest",
+              sp_cost: 0, target: "self", buff_stat: "MP", dice: "hit_die"})
 Repo.insert!(%Skill{name: "Double Attack", level_req: 1, type: "damage",
               dice: "1d6", sp_cost: 1, target: "enemy"})
+Repo.insert!(%Skill{name: "Rage", level_req: 1, type: "buff", sp_cost: 2,
+              target: "self", buff_stat: "STR", dice: "4d1"})
 
 ###### CLASSES #######
 barbarian_profs = ["Athletics", "Intimidation"]
@@ -76,24 +84,39 @@ Repo.insert!(%Classes.Skills{class: Classes.get_class_by_name("Wizard"),
                               skill: Skills.get_skill_by_name("Short Rest")})
 Repo.insert!(%Classes.Spells{class: Classes.get_class_by_name("Wizard"),
                               spell: Spells.get_spell_by_name("Magic Missle")})
+Repo.insert!(%Classes.Spells{class: Classes.get_class_by_name("Wizard"),
+                              spell: Spells.get_spell_by_name("Fire Bolt")})
 
 ###### ARMORS ######
 Repo.insert!(%Armor{name: "Mage Armor", base: 13, armor_category: "Special"})
 Repo.insert!(%Armor{name: "Leather", base: 11, armor_category: "Light"})
+Repo.insert!(%Armor{name: "Breatplate", base: 12, armor_category: "Light"})
 Repo.insert!(%Armor{name: "Plate", base: 18, armor_category: "Heavy",
             max_dex_bonus: 0, stealth_disadvantage: true, str_minimum: 15})
+Repo.insert!(%Armor{name: "Chain Mail", base: 16, armor_category: "Heavy",
+            max_dex_bonus: 0, stealth_disadvantage: true, str_minimum: 13})
+Repo.insert!(%Armor{name: "Ring Mail", base: 14, armor_category: "Heavy",
+            max_dex_bonus: 0, stealth_disadvantage: true})
+Repo.insert!(%Armor{name: "Half Plate", base: 15, armor_category: "Medium",
+            max_dex_bonus: 2, stealth_disadvantage: true})
+Repo.insert!(%Armor{name: "Breatplate", base: 14, armor_category: "Medium",
+            max_dex_bonus: 2})
 
 ###### ATTACKS ######
 Repo.insert!(%Attack{name: "Pierce", damage_dice: "1d4", target: "enemy",
-                      type: "normal"})
+                      type: "pierce"})
 Repo.insert!(%Attack{name: "Cleave", damage_dice: "1d8", target: "enemy",
-                      type: "normal"})
+                      type: "slash"})
+Repo.insert!(%Attack{name: "Lunge", damage_dice: "1d10", target: "enemy",
+                      type: "slash"})
 
 ###### WEAPONS ######
 Repo.insert!(%Weapon{name: "Dagger", weapon_category: "Simple",
                     attack: Attacks.get_attack_by_name("Pierce")})
 Repo.insert!(%Weapon{name: "Battleaxe", weapon_category: "Martial",
                     attack: Attacks.get_attack_by_name("Cleave")})
+  Repo.insert!(%Weapon{name: "Halberd", weapon_category: "Martial",
+                      attack: Attacks.get_attack_by_name("Lunge")})
 
 ###### USERS ######
 Repo.insert!(%User{email: "Cristian", admin: true, password_hash: pwhash})
