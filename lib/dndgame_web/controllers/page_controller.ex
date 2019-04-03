8a752f1ea74ae4438354979751c178d30aaf13de
csstransky <csstransky@gmail.com>
2019-04-02 23:13:19 -0400

    In the middle of dealing with dupe character picks

--- lib/dndgame_web/controllers/page_controller.ex
+++ lib/dndgame_web/controllers/page_controller.ex
@@ -2,8 +2,21 @@ defmodule DndgameWeb.PageController do
   use DndgameWeb, :controller
 
   # Make this match with %{"gameName" => gameName, "playerName" => playerName}
-  def game(conn, %{"user_id" => user_id, "world" => world}) do
-    gameName = world
+  def game(conn, %{"user_id" => user_id, "world_name" => world_name,
+    "party_id_1" => party_id_1,
+    "party_id_2" => party_id_2,
+    "party_id_3" => party_id_3}) do
+    IO.inspect(conn)
+    if (party_id_1 == party_id_2 && party_id_1 != "")
+      || (party_id_1 == party_id_3 && party_id_1 != "")
+      || (party_id_2 == party_id_3 && party_id_2 != "") do
+        IO.inspect("What up my dude")
+    end
+    IO.inspect(party_id_1)
+    IO.inspect(party_id_2)
+    IO.inspect(party_id_3)
+    IO.inspect(world_name)
+    gameName = world_name
     playerName = user_id
     render conn, "game.html", gameName: gameName, playerName: playerName
   end
