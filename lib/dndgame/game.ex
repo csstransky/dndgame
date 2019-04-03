1ad43d820e8ce7fa13211211092f45b9f0dc95fa
csstransky <csstransky@gmail.com>
2019-04-03 19:49:13 -0400

    Making the base gamestate

--- lib/dndgame/game.ex
+++ lib/dndgame/game.ex
@@ -1,8 +1,42 @@
 defmodule Dndgame.Game do
-  def new do
+  @starting_x 44
+  @starting_y 64
+  @boss_x 40
+  @boss_y 40
+  def new(party_id_1, party_id_2, party_id_3) do
     %{
-      willBeCompleted: true,
-      isDone: false,
+        playerPosn: %{
+          x: @starting_x
+          y: @starting_y
+          direction: 0 # 0 up, 1 right, 2 down, 3 left
+        }
+        battleParty: []
+        staticParty: [] # TODO: fill this
+        monsters: [] # fills up when character encounters monsters
+        boss: %{
+          x: @boss_x
+          y: @boss_y
+        }
+        orderArray: [] # fills up with strings of whose turn it is
+        orderIndex: 0 # who's turn it is
+
+        isLevelUp: [0,0,0] # flag to show a level screen or not
+        isBossDead: "" # name of person who killed boss
+        isGameOver: false # flag to show a gameover screen
+
+        currentMenu: "main"
+        menuIndex: 0
+        characterAction: ""
+        monsterAction: ""
     }
+  end
+
+  def client_view(game) do
+    # TODO: fix this up ion the future
+    game
+  end
+
+  def roll_dice(dieString) do
+    
   end
 end
